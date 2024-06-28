ac1eb30bba31632040f4b56856052fe692d86770
Demel <iamdemel@outlook.com>
2024-06-28 06:41:39 -0700

    Created the base to add in list of locations and parish information on Home Page via json file.

--- static/JS code/general.js
+++ static/JS code/general.js
@@ -75,7 +75,7 @@ function Bean_Tuning(bean) {
   bean.style.animationDelay = (Math.random() * 20) + 's';
   
   if(current_page.classList.contains('home')){
-  bean.style.setProperty('--end_pos', '5790px');
+  bean.style.setProperty('--end_pos', '9935px');
   }
   else if(current_page.classList.contains('about')){
     bean.style.setProperty('--end_pos', '630px');
